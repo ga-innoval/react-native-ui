641b9889a320777df55c7f30716b0167473cd0b2
Rigoberto Rascon <rigobertorascon5@gmail.com>
2023-02-22 15:41:28 -0700

    chore: chip button unit tests

---
+++ src/components/__tests__/Chip.Button.test.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react-native';
+import { ChipButton } from '../Chip.Button';
+
+const eventData = {
+  nativeEvent: {
+    pageX: 20,
+    pageY: 30,
+  },
+};
+
+describe('<ChipButton />', () => {
+  let component: any = null;
+  const mockFn = jest.fn();
+
+  beforeEach(() => {
+    component = render(
+      <ChipButton text="Press me" iconName="account" onPress={mockFn} />
+    );
+  });
+
+  it('should render correctly', () => {
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should call onPress function', () => {
+    const button = screen.getByText(/press/i);
+    fireEvent.press(button, eventData);
+    expect(mockFn).toBeCalledTimes(1);
+  });
+});
